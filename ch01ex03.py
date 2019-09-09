c03c4175aa1d37af9681088995c86314a8d9bdc6
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-09-09 16:03:18 +0900

    기본입출력(몫과 나머지)

---
+++ ch01ex03.py
@@ -0,0 +1,15 @@
+# 기본입출력
+# 몫과 나머지
+
+num1 = int(input("input num1: "))
+num2 = int(input("input num2: "))
+
+print()
+print("num1 / num2 = %.1f" % (num1 / num2))
+print("quotient: %d" % (num1 // num2))
+print("remainder: %d" % (num1 % num2))
+
+print()
+print("num1 / num2 = {}".format(num1 / num2))
+print("quotient: {}".format(num1 // num2))
+print("remainder: {}".format(num1 % num2))
