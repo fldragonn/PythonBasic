2962ae8e1c9687e3b4270337127656a11f0f4d40
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-09-18 16:54:12 +0900

    함수응용(한 변의 길이가 n픽셀 도형그리기)

--- ch02ex02.py
+++ ch02ex02.py
@@ -1,9 +1,10 @@
 # step2. n px 도형 그리기
 # drawRect(n), drawTri(n)
+# 길이 초기 값 설정하기 (n = 50)
 
 import turtle as t
 
-def drawRect(n):
+def drawRect(n=50):
     for x in range(4):
         t.forward(n)
         t.right(90)
@@ -14,8 +15,5 @@ def drawTri(n):
         t.right(120)
 
 drawRect(100)
-
-t.up()
-t.goto(100, 0)
-t.down()
+drawRect()
 drawTri(50)
