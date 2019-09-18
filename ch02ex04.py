e9ac5dfdc17b4b530bc77883255f79ff1f53288d
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-09-18 17:16:14 +0900

    함수응용(n각형 그리기)

---
+++ ch02ex04.py
@@ -0,0 +1,20 @@
+# 도형 함수 만들기
+# moveT(x, y)
+# drawPoly(n, d, x, y)
+
+import turtle as t
+
+def moveT(x, y):
+    t.up()
+    t.goto(x, y)
+    t.down()
+
+def drawPoly(n=4, d=50, x=0, y=0):
+    moveT(x, y)
+    for x in range(n):
+        t.forward(d)
+        t.right(360/n)
+
+drawPoly()
+drawPoly(8, 30, 100, 100)
+drawPoly(5, 50)
