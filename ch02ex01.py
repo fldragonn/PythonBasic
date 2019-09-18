d65101cc35825d3b51781b85fd9bde2a34df450c
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-09-18 17:21:55 +0900

    함수응용(도형 그리기)

--- ch02ex01.py
+++ ch02ex01.py
@@ -1,25 +1,22 @@
 # 도형 함수 만들기
-# step3. 특정 위치에 도형 그리기
+# step1. 한 변이 50px인 도형 그리기
 # drawRect, drawTri
 
 import turtle as t
 
-def drawRect(x=0, y=0, n=50):
-    t.up()
-    t.goto(x, y)
-    t.down()
-
+def drawRect():
     for x in range(4):
-        t.forward(n)
+        t.forward(50)
         t.right(90)
 
-def drawTri(x=0, y=0, n=50):
-    t.up()
-    t.goto(x, y)
-    t.down()
+def drawTri():
     for x in range(3):
-        t.forward(n)
+        t.forward(50)
         t.right(120)
 
-drawRect(100, 100, 100)
-drawTri(-100, -100, 200)
+drawRect()
+
+t.up()
+t.goto(100, 0)
+t.down()
+drawTri()
