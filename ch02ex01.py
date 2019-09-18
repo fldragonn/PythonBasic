02e260a07fe6003666731346536165113c35c930
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-09-18 16:49:23 +0900

    함수응용(도형그리기 함수)

---
+++ ch02ex01.py
@@ -0,0 +1,22 @@
+# 도형 함수 만들기
+# step1. 한 변이 50px인 도형 그리기
+# drawRect, drawTri
+
+import turtle as t
+
+def drawRect():
+    for x in range(4):
+        t.forward(50)
+        t.right(90)
+
+def drawTri():
+    for x in range(3):
+        t.forward(50)
+        t.right(120)
+
+drawRect()
+
+t.up()
+t.goto(100, 0)
+t.down()
+drawTri()
