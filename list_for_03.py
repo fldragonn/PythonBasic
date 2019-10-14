74faaafa1c1987afdf3ec3e4df32d72dddfe604c
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-10-14 16:09:42 +0900

    리스트 예제04. 학생 성적표 점수 수정하기

--- list_for_03.py
+++ list_for_03.py
@@ -11,9 +11,24 @@ grades = [95, 75, 85, 65]
 print("Grages: ", grades)
 
 # 각 성적을 수정해 출력하기
+print("각 성적을 5점씩 추가해 출력하기")
 for value in grades:
     value = value + 5
     print(value)
 
 # 원본 리스트 출력하기
+print("Grages: ", grades)
+
+# 원본 리스트 자료 값 수정하기
+print()
+print("실제 리스트 원소 값 수정하기")
+
+# 리스트 원소 개수 구하기
+cnt = len(grades)
+
+# 각 학생 성적 5점씩 추가하기
+for idx in range(cnt):
+    grades[idx] = grades[idx] + 5
+
+# 수정 리스트 출력하기
 print("Grages: ", grades)
